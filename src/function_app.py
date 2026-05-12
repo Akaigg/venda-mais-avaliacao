ec3cf0bb8c516145f06af7d227856e112656f354
MATHEUS KARPINSKI <matheus.karpinski@univille.br>
2026-05-12 19:44:34 -0300

    feat: criação das functions de extração

--- src/function_app.py
+++ src/function_app.py
@@ -20,3 +20,23 @@ def extract_fornecedor(myTimer: func.TimerRequest) -> None:
               use_monitor=False) 
 def extract_pedido(myTimer: func.TimerRequest) -> None:
     logging.info('tabela pedidos')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def categoria_produto(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function executed.')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_entrega(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function executed.')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_estoque_movimentacao(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function executed.')
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def extract_estoque_saldo(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function executed.')

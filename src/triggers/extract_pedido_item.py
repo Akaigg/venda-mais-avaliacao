4d96f9a5a7ba994b01b6f66885dfd19ac5b49578
VITOR MACHADO BLUME <vitor.blume@univille.br>
2026-05-21 21:31:47 -0300

    feat: conexão

--- src/triggers/extract_pedido_item.py
+++ src/triggers/extract_pedido_item.py
@@ -1,8 +1,15 @@
 import logging
 import azure.functions as func
+import os
 
 bp = func.Blueprint()
 @bp.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def extract_pedido_item(myTimer: func.TimerRequest) -> None:
-    logging.info('Python timer trigger function executed.')
+
+    sql_server = os.getenv("SQL_SERVER_SOURCE")
+    sql_database = os.getenv("SQL_DATABASE_SOURCE")
+    sql_user = os.getenv("SQL_USER_SOURCE")
+    sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
+
+    logging.info(f"""servidor: {sql_server}, banco: {sql_database}, usuario: {sql_user}, senha: {sql_pass}""")

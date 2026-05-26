bded435db08bf3e22351920789f7d8607991af1f
VITOR MACHADO BLUME <vitor.blume@univille.br>
2026-05-26 19:36:27 -0300

    feat: Teste pydbc

--- src/triggers/extract_categoria_produto.py
+++ src/triggers/extract_categoria_produto.py
@@ -1,15 +1,16 @@
 import logging
 import azure.functions as func
 import os
+import pyodbc
 
 bp = func.Blueprint()
 @bp.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def extract_categoria_produto(myTimer: func.TimerRequest) -> None:
 
-    sql_server = os.getenv("SQL_SERVER_SOURCE")
-    sql_database = os.getenv("SQL_DATABASE_SOURCE")
-    sql_user = os.getenv("SQL_USER_SOURCE")
-    sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
+    sql_server = 'SQL_SERVER_SOURCE'
+    sql_database = 'SQL_DATABASE_SOURCE'
+    sql_user = 'SQL_USER_SOURCE'
+    sql_pass = 'SQL_PASSWORD_SOURCE'
 
     logging.info(f"""servidor: {sql_server}, banco: {sql_database}, usuario: {sql_user}, senha: {sql_pass}""")

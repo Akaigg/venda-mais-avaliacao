19ec2189aa958de5b093717ce7424b3d139c0af8
Akaigg <vitorblumeakai@gmail.com>
2026-05-28 18:04:36 -0300

    ajuste: conexao pyodbc para rodar na azure

--- src/triggers/extract_categoria_produto.py
+++ src/triggers/extract_categoria_produto.py
@@ -1,11 +1,11 @@
 import logging
 import azure.functions as func
 import os
-import pymssql
+import pyodbc
 
 bp = func.Blueprint()
-@bp.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
+
+@bp.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
 def extract_categoria_produto(myTimer: func.TimerRequest) -> None:
 
     sql_server = os.getenv('SQL_SERVER_SOURCE')
@@ -13,7 +13,21 @@ def extract_categoria_produto(myTimer: func.TimerRequest) -> None:
     sql_user = os.getenv('SQL_USER_SOURCE')
     sql_pass = os.getenv('SQL_PASSWORD_SOURCE')
     
-    conn = pymssql.connect(server=sql_server, user=sql_user, password=sql_pass, database=sql_database)
-
-    logging.info(f"""servidor: {sql_server}, banco: {sql_database}, usuario: {sql_user}, senha: {sql_pass}""")
+    sql_driver = "ODBC Driver 18 for SQL Server"
     
+    connection_string = (
+        f"DRIVER={{{sql_driver}}};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_database};"
+        f"UID={sql_user};"
+        f"PWD={sql_pass};"
+    )
+
+    logging.info(f"Iniciando tentativa de conexão no banco: {sql_database}")
+    
+    try:
+        with pyodbc.connect(connection_string) as conn:
+            with conn.cursor() as cursor:
+                logging.info("Sucesso! Conexão estabelecida com pyodbc na Azure.")             
+    except Exception as e:
+        logging.error(f"Erro ao conectar com o banco de dados via pyodbc: {str(e)}")
